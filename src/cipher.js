e4c89acb22524b3628637e8d5622d3ffcbce929c
Monse Salas <cmonse.salasv@gmail.com>
2020-04-14 10:13:36 -0500

    reside;ado

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,13 @@
 window.cipher = {
-  encode:(textoACifrar, clave)=>{
+  encode: (textoACifrar, clave) => {
     //let esValido = validacion(); //si no hay espacios en clave/texto continua
     let letraASCII;
     let formula;
     let asciiALetra;
-    let textoCifrado="";
-    for (var i = 0; i < textoACifrar.length; i++) {  //Lee leetra por letra
-      letraASCII = textoACifrar.charCodeAt(i);    // 1Traduce al ascii letra por letra
-      formula = (((letraASCII - 65 + parseInt(clave))%26)+65); //formula para recorrer los desplazamientos de letra x letra
+    let textoCifrado = "";
+    for (var i = 0; i < textoACifrar.length; i++) { //Lee leetra por letra
+      letraASCII = textoACifrar.charCodeAt(i); // 1Traduce al ascii letra por letra
+      formula = (((letraASCII - 65 + parseInt(clave)) % 26) + 65); //formula para recorrer los desplazamientos de letra x letra
 
       if (letraASCII === 32) { // Si el valor de letraASCII es un ESPACIO
         formula = 32;
@@ -17,15 +17,15 @@ window.cipher = {
     }
     return textoCifrado;
   },
-  decode:(textoACifrar, clave )=>{
+  decode: (textoACifrar, clave) => {
     //let esValido = validacion(); //si no hay espacios en clave/texto continua
     let letraASCII;
     let formula;
     let asciiALetra;
-    let textoCifrado="";
-    for (var i = 0; i < textoACifrar.length; i++) {  //Lee leetra por letra
-      letraASCII = textoACifrar.charCodeAt(i);    // 1Traduce al ascii letra por letra
-      formula = (((letraASCII + 65 - parseInt(clave))%26)+65); //formula para recorrer los desplazamientos de letra x letra
+    let textoCifrado = "";
+    for (var i = 0; i < textoACifrar.length; i++) { //Lee leetra por letra
+      letraASCII = textoACifrar.charCodeAt(i); // 1Traduce al ascii letra por letra
+      formula = (((letraASCII + 65 - parseInt(clave)) % 26) + 65); //formula para recorrer los desplazamientos de letra x letra
 
       if (letraASCII === 32) { // Si el valor de letraASCII es un ESPACIO
         formula = 32;
@@ -33,7 +33,7 @@ window.cipher = {
       asciiALetra = String.fromCharCode(formula); // valor ascii a letra
       textoCifrado = textoCifrado + asciiALetra; //crear una variable que guarde mis letras
     }
-    console.log(textoCifrado);
+    //console.log(textoCifrado);
     return textoCifrado;
   }
 };
